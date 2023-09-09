fe213c0f3509b7e4077519ec4642eb4599ffd239
mimunojun <mimura.tsukuba@gmail.com>
2023-09-10 00:00:08 +0900

    validation

--- booking-app/main.go
+++ booking-app/main.go
@@ -9,10 +9,6 @@ func main() {
 	conferenceName := "Go Conference"
 	const conferenceTickets int = 50
 	var remainingTickets uint = 50
-	var firstName string
-	var lastName string
-	var email string
-	var userTickets uint
 
 	//var bookings []string
 	var bookings = []string{}
@@ -24,7 +20,12 @@ func main() {
 	fmt.Printf("We have total of %v tickets and %v are still available\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
 
-	for {
+	for remainingTickets > 0 && len(bookings) < 50 {
+		var firstName string
+		var lastName string
+		var email string
+		var userTickets uint
+
 		fmt.Println("Enter your first name: ")
 		fmt.Scan(&firstName)
 
@@ -37,7 +38,11 @@ func main() {
 		fmt.Println("Enter number of tickets: ")
 		fmt.Scan(&userTickets)
 
-		if userTickets < remainingTickets {
+		var isValidName bool = len(firstName) >= 2 && len(lastName) >= 2
+		var isValidEmail bool = strings.Contains(email, "@")
+		var isValidTicketNumber bool = userTickets > 0 && userTickets <= remainingTickets
+
+		if isValidName && isValidEmail && isValidTicketNumber {
 			remainingTickets = remainingTickets - userTickets
 			bookings = append(bookings, firstName+" "+lastName)
 
@@ -54,15 +59,19 @@ func main() {
 			var noTicketsRemaining bool = remainingTickets == 0
 
 			if noTicketsRemaining {
-				//end program
 				fmt.Println("Our conference is booked out. Come back next year.")
 				break
 			}
-		} else if userTickets == remainingTickets {
-			//do something else
-
 		} else {
-			fmt.Printf("We only have %v tickets remaining, so you can't book %v tickets.\n", remainingTickets, userTickets)
+			if !isValidName {
+				fmt.Println("first name or last name entered is too short")
+			}
+			if !isValidEmail {
+				fmt.Println("email address you entered doesn't contain @ sign")
+			}
+			if !isValidTicketNumber {
+				fmt.Println("number of tickets you entered is invalid")
+			}
 
 		}
 
